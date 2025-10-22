6ce4f2eaf71ebee9bfdc353903fcc594de509814
manuel latorre <manuel.latorre11@gmail.com>
2025-10-22 09:45:18 -0300

    feat(auth): implement session management with createSession and getSession functions

---
+++ apps/web/app/(auth)/actions/session.ts
@@ -0,0 +1,44 @@
+"use server"
+
+import { jwtVerify, SignJWT } from "jose";
+import { Session } from "../types/session";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+const secretKey = process.env.SESSION_SECRET_KEY!;
+const encodedKey = new TextEncoder().encode(secretKey);
+
+export async function createSession(payload: Session) {
+  const expiredAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
+
+  const session = await new SignJWT(payload)
+    .setProtectedHeader({ alg: "HS256" })
+    .setIssuedAt()
+    .setExpirationTime("7d")
+    .sign(encodedKey);
+
+  (await cookies()).set("session", session, {
+    httpOnly: true,
+    secure: true,
+    expires: expiredAt,
+    sameSite: "lax",
+    path: "/",
+  });
+}
+
+export async function getSession() {
+  const cookie = (await cookies()).get("session")?.value;
+
+  if (!cookie) return null;
+
+  try {
+    const { payload } = await jwtVerify(cookie, encodedKey, {
+      algorithms: ["HS256"],
+    });
+
+    return payload as Session;
+  } catch (error) {
+    console.error("fAILED to verify the session", error);
+    redirect("/login");
+  }
+}
