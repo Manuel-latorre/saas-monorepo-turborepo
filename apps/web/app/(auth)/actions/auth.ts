42de7a795064dd73edcf69ca47e39fd10aa29774
manuel latorre <manuel.latorre11@gmail.com>
2025-10-22 08:59:32 -0300

    feat(auth): add signup and login functions with validation and error handling

---
+++ apps/web/app/(auth)/actions/auth.ts
@@ -0,0 +1,90 @@
+"use server";
+
+import { BACKEND_URL } from "@/lib/constants";
+import { redirect } from "next/navigation";
+import { FormState } from "../types/types";
+import { LoginFormSchema, SignupFormSchema } from "../types/schemas/auth";
+
+export async function signup(
+  state: FormState,
+  formData: FormData
+): Promise<FormState> {
+  const valiationFields = SignupFormSchema.safeParse({
+    name: formData.get("name"),
+    email: formData.get("email"),
+    password: formData.get("password"),
+  });
+
+  if (!valiationFields.success) {
+    const fieldErrors = valiationFields.error.flatten().fieldErrors;
+    return {
+      error: {
+        name: fieldErrors.name?.[0],
+        email: fieldErrors.email?.[0],
+        password: fieldErrors.password,
+      },
+      message: "Validation failed",
+    };
+  }
+
+  const response = await fetch(`${BACKEND_URL}/auth/signup`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(valiationFields.data),
+  });
+
+  console.log("RESPOMSE---->>", response);
+
+  if (response.ok) {
+    redirect("/login");
+  } else
+    return {
+      message:
+        response.status === 409
+          ? "The user is already registered!"
+          : response.statusText,
+    };
+}
+
+export async function login(
+  state: FormState,
+  formData: FormData
+): Promise<FormState> {
+  const validatedFields = LoginFormSchema.safeParse({
+    email: formData.get("email"),
+    password: formData.get("password"),
+  });
+
+  if (!validatedFields.success) {
+    const fieldErrors = validatedFields.error.flatten().fieldErrors;
+    return {
+      error: {
+        email: fieldErrors.email?.[0],
+        password: fieldErrors.password,
+      },
+      message: "Validation failed",
+    };
+  }
+
+  const response = await fetch(`${BACKEND_URL}/auth/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(validatedFields.data),
+  })
+
+
+  if(response.ok){
+    const result = await response.json();
+    //TODO: Create Session for Auth user
+    console.log({result});
+  }
+  else {
+    return {
+        message: response.status === 401 ? "Invalidad Credentials" : response.statusText
+    }
+  }
+}
