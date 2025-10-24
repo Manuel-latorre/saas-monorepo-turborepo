31baa0a0372399dea90db9a19310cdbe25295bdd
manuel latorre <manuel.latorre11@gmail.com>
2025-10-24 20:02:15 -0300

    feat(auth): implement refresh token validation and generation methods in AuthService

--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -76,4 +76,26 @@ export class AuthService {
 
     return currentUser
   }
+
+  async validateRefreshToken(userId:string){
+    const user = await this.userService.findOne(userId)
+
+    if(!user) throw new UnauthorizedException("User not found!");
+
+    const currentUser = { id: user.id };
+
+    return currentUser
+  }
+
+
+  async refreshToken(userId:string, name:string){
+    const { accessToken, refreshToken } = await this.generateTokens(userId);
+    
+    return {
+      id: userId,
+      name: name,
+      accessToken,
+      refreshToken
+    };
+  }
 }
