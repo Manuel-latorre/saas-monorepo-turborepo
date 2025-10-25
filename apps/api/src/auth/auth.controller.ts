cbec6cc3032a9ac5c79f1dc077864cd9490f2db4
manuel latorre <manuel.latorre11@gmail.com>
2025-10-25 00:39:43 -0300

    feat(auth): enhance Google callback to redirect with user tokens after successful authentication

--- apps/api/src/auth/auth.controller.ts
+++ apps/api/src/auth/auth.controller.ts
@@ -1,50 +1,60 @@
-import { Body, Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Request,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from '../user/dto/create-user.dto';
 import { LocalAuthGuard } from './guards/local-auth/local-auth.guard';
 import { JwtAuthGuard } from './guards/jwt-auth/jwt-auth.guard';
 import { RefreshAuthGuard } from './guards/refresh-auth/refresh-auth.guard';
 import { GoogleAuthGuard } from './guards/google-auth/google-auth.guard';
+import { type Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-    @Post("signup")
-    registerUser(@Body() CreateUserDto: CreateUserDto){
-      return this.authService.registerUser(CreateUserDto)
-    } 
+  @Post('signup')
+  registerUser(@Body() CreateUserDto: CreateUserDto) {
+    return this.authService.registerUser(CreateUserDto);
+  }
 
-    @UseGuards(LocalAuthGuard)    
-    @Post("login")
-    login(@Request() req){
-      return this.authService.login(req.user.id, req.user.name)
-    }
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  login(@Request() req) {
+    return this.authService.login(req.user.id, req.user.name);
+  }
 
+  @UseGuards(JwtAuthGuard)
+  @Get('protected')
+  getAll(@Request() req) {
+    return {
+      message: `Now you can access this protected API. This is your user ID: ${req.user.id}`,
+    };
+  }
 
-    @UseGuards(JwtAuthGuard)
-    @Get("protected")
-    getAll(@Request() req){
-      return {message: `Now you can access this protected API. This is your user ID: ${req.user.id}` }
-    }
+  @UseGuards(RefreshAuthGuard)
+  @Post('refresh')
+  refreshToken(@Request() req) {
+    return this.authService.refreshToken(req.user.id, req.user.name);
+  }
 
+  @UseGuards(GoogleAuthGuard)
+  @Get('google/login')
+  googleLogin() {}
 
-    @UseGuards(RefreshAuthGuard)
-    @Post("refresh")
-    refreshToken(@Request() req){
-      return this.authService.refreshToken(req.user.id, req.user.name);
-    }
+  @UseGuards(GoogleAuthGuard)
+  @Get('google/callback')
+  async googleCallback(@Request() req, @Res() res: Response) {
+    //console.log("GOOGLE USER--->:", req.user);
 
-
-    @UseGuards(GoogleAuthGuard)
-    @Get("google/login")
-    googleLogin(){}
-
-
-    @UseGuards(GoogleAuthGuard)
-    @Get("google/callback")
-    googleCallback(@Request() req){
-      console.log("GOOGLE USER--->:", req.user);
-      
-    }
-
+    const response = await this.authService.login(req.user.id, req.user.name);
+    res.redirect(
+      `http://localhost:3001/api/auth/google/callback?userId=${response.id}&name=${response.name}&accessToken=${response.accessToken}&refreshToken=${response.refreshToken}`,
+    );
+  }
 }
