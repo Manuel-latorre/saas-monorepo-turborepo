fb19f666efd94965f4c7ca82667d6ceb4cd7a915
manuel latorre <manuel.latorre11@gmail.com>
2025-10-25 17:50:03 -0300

    feat(auth): include user role in session creation for enhanced role-based access control

--- apps/web/app/api/auth/google/callback/route.ts
+++ apps/web/app/api/auth/google/callback/route.ts
@@ -1,4 +1,5 @@
 import { createSession } from "@/app/(auth)/actions/session";
+import { Role } from "@/app/(auth)/types/types";
 import { redirect } from "next/navigation";
 import { NextRequest } from "next/server";
 
@@ -9,14 +10,16 @@ export async function GET(req: NextRequest) {
   const refreshToken = searchParams.get("refreshToken");
   const userId = searchParams.get("userId");
   const name = searchParams.get("name");
+  const role = searchParams.get("role");
 
-  if (!accessToken || !refreshToken || !userId || !name)
+  if (!accessToken || !refreshToken || !userId || !name || !role)
     throw new Error("Google Oauth failed!");
 
   await createSession({
     user: {
       id: userId,
       name: name,
+      role: role as Role
     },
     accessToken,
     refreshToken,
